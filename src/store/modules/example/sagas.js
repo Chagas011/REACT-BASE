a770f5bae792eb29a25a70c917d3a78d613529d1
Chagas011 <washington.chagas.9@hotmail.com>
2024-04-15 23:59:33 -0300

    utilizando redux-saga

--- src/store/modules/example/sagas.js
+++ src/store/modules/example/sagas.js
@@ -1,4 +1,5 @@
 import { call, put, all, takeLatest } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 import * as actions from './actions';
 import * as types from '../types';
 
@@ -6,14 +7,16 @@ const requisicao = () =>
   new Promise((resolve, reject) => {
     setTimeout(() => {
       resolve();
-    }, 2000);
+    }, 600);
   });
 
 function* exampleRequest() {
   try {
     yield call(requisicao);
     yield put(actions.clicaBotaoSuccess());
+    toast.success('deu bom');
   } catch {
+    toast.error('deu erro');
     yield put(actions.clicaBotaoFailure());
   }
 }
